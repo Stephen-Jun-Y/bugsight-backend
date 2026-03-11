f90685f8490d2a50a95fdba8d7479c08121e137c
Stephen-Jun-Y <1962168057@qq.com>
2026-03-11 18:40:43 +0800

    refactor: align backend endpoints with frontend api spec

---
+++ src/main/java/com/bugsight/dto/request/ChangePasswordRequest.java
@@ -0,0 +1,16 @@
+package com.bugsight.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class ChangePasswordRequest {
+
+    @NotBlank(message = "当前密码不能为空")
+    private String currentPassword;
+
+    @NotBlank(message = "新密码不能为空")
+    @Size(min = 6, max = 30, message = "密码长度为6-30个字符")
+    private String newPassword;
+}
